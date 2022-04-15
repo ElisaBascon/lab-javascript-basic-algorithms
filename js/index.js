8f8b36a4c35d8c9abb363f400145421af7a794d0
elisa <bascongarcia@gmail.com>
2022-04-15 20:17:41 +0200

    final whitout the las solution

--- js/index.js
+++ js/index.js
@@ -1,6 +1,40 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+// Iteration 1: Names and Input //
+const hacker1 = 'Elisa';
+console.log(`The driver"s name is ${hacker1}`);
+
+const hacker2 = 'Pepe';
+console.log(`The navigator"s name is ${hacker2}`);
 
 
-// Iteration 3: Loops
+// Iteration 2: Conditionals //
+
+if (hacker2.length > hacker1.length) {
+    console.log(`The driver has the longest name, it has ${hacker2.length} characters.`);
+}
+else if (hacker2.length < hacker1.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker1.length} characters.`);
+}
+else {
+    console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`);
+}
+
+// Iteration 3: Loops //
+const splittedName = hacker1.split('');
+const nameWithSpaces = splittedName.join(' ');
+const upperCaseNameWithSpaces = nameWithSpaces.toUpperCase();
+console.log(upperCaseNameWithSpaces);
+
+console.log(splittedName.reverse().join(' ')); 
+
+//NO ME SALE ESTA TERCERA PARTE//
+const allNames = ['Pepe', 'Elisa'];
+const orderNames = allNames.sort(a, b)=> {
+  if (a > b) {
+    console.log('The driver"s name goes first.')
+  }
+  else if (a < b) {
+    console.log('Yo, the navigator goes first definitely.')
+  }
+  else (a = b) {
+    console.log('What?! You both have the same name?')
+  }
